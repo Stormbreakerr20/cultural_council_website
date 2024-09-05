0dfdca784582c324e451c3f062dc13b2c86d69f8
Stormbreaker20 <luvsharma.del@gmail.com>
2024-09-06 00:36:55 +0530

    Updated

---
+++ app/(root)/showcase/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-start lg:items-center h-full mt-20 max-lg:mt-32"></div>
+  );
+};
+
+export default page;
--- components/shared/LandingSection.tsx
+++ components/shared/LandingSection.tsx
@@ -3,7 +3,7 @@ import { MainCarousel } from "./MainCarousel";
 function LandingSection() {
   return (
     <>
-      <div className="flex flex-col w-full justify-center items-center mt-48 px-5 md:px-12">
+      <div className="flex flex-col w-full justify-center items-center mt-48">
         <div className="flex flex-col items-center">
           <div className="text-xl font-medium text-neutral-100/[0.7] text-center">
             This is the Cultural Society, IIT Mandi
@@ -14,6 +14,25 @@ function LandingSection() {
         </div>
         <div className="mt-24 px-12 w-full max-w-screen-lg mb-20">
           <MainCarousel />
+        </div>
+        <div className="w-[80%] max-sm:w-full text-center">
+          <div className="mb-8">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold">
+              About The Council
+            </h1>
+          </div>
+          <p className="text-sm md:text-base lg:text-lg mb-20">
+            The Cultural Council at IIT Mandi is a vibrant hub for students
+            passionate about arts, culture, and creativity. It plays a pivotal
+            role in fostering a diverse cultural atmosphere on campus,
+            organizing events that range from music and dance performances to
+            drama and literary activities. The council provides a platform for
+            students to showcase their talents, learn new skills, and engage in
+            cultural exchanges, thereby enriching the campus experience. By
+            promoting various cultural activities, the Cultural Council helps in
+            building a close-knit community where students can explore their
+            interests and celebrate the rich diversity of cultural expressions.
+          </p>
         </div>
       </div>
     </>
